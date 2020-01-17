c5117b321ef2429840d684b472e1f3533fd96314
AJ Keeter <aleckeeter94@gmail.com>
2020-01-17 14:57:16 -0600

    Adds about page to application

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -15,17 +15,17 @@ const Users =({ users, loading }) => {
         </div>
       );
     }
-}
+};
 
 Users.propTypes = {
     users: PropTypes.array.isRequired,
     loading: PropTypes.bool.isRequired
-}
+};
 
 const userStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
     gridGap: '1rem'
-}
+};
 
 export default Users;
